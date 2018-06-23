1ef95db59e1fa95592216355745fdb5217035dda
David Tenenbaum <github email address>
2018-06-23 10:39:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  if (someValue <= 400) {
+    return 'This one is on me!';
+  } else if (someValue < 2500){
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
+}
+
+function ternaryCheckCity(city) {
+  let response;
+  city === 'NYC' ? (response = 'Ok, sounds good.') :  (response = 'No go.');
+  return response;
+}
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    case 'thanks for everything':
+      return 'Bye.';
+      break;
+  }
+}
