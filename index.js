44300c56af51c7ef0301b803522d18b3fa67b859
Kayla Fitzgerald <fitzgerald.km11@gmail.com>
2018-06-23 17:01:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feet){
+  let result;
+  if (feet <= 400) {
+    result = 'This one is on me!';
+  }
+  else if (1999 < feet && feet < 2500) {
+    result = 'I will gladly take your thirty bucks.';
+  }
+  else {
+    result = 'No can do.';
+  }
+return result;
+}
+
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+}
+
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
+}
